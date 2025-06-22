a1b5dfaeec805d8d009eb590af47c5ac7917a999
Pedro Oliveira <pedroaolive@gmail.com>
2025-06-22 17:10:31 -0300

    inital commit

---
+++ src/pages/blog/posts/[slug].tsx
@@ -0,0 +1,9 @@
+const PostPage = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+    </div>
+  );
+};
+
+export default PostPage;
---
+++ src/pages/blog/posts/index.tsx
@@ -0,0 +1,9 @@
+const PostsPage = () => {
+  return (
+    <div>
+      <h1>Blog Post</h1>
+    </div>
+  );
+};
+
+export default PostsPage;
