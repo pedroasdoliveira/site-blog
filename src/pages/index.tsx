be543c18c3ec9e831ee1a0eb969e8bc2363b97f0
Pedro Oliveira <pedroaolive@gmail.com>
2025-06-22 18:34:03 -0300

    feat: create Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/Header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -12,8 +13,12 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div>
-      
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px]
+      items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20
+      font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
     </div>
   );
 }
