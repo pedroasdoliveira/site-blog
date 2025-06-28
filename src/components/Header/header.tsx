7e3cb62bc1dd78ebe4ffabc8d7b816290895a541
Pedro Oliveira <pedroaolive@gmail.com>
2025-06-28 17:51:55 -0300

    style: modify styles in Header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { ActiveLink } from "../Active-Link";
+import { Logo } from "../Logo";
 
 export const Header = () => {
   return (
-    <header
-      className="
-     fixed top-0 z-50 w-full border-b
-   border-white/10 bg-background/95 backdrop-blur 
-     supports-[backdrop-filter]:bg-background/60"
-    >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
         <div className="flex h-16 items-center justify-between">
-          <Link href={"/"}>Logo</Link>
+          <Logo />
 
           <nav className="flex items-center gap-6">
             <ActiveLink href={"/"}>Início</ActiveLink>
@@ -20,9 +16,7 @@ export const Header = () => {
             <ActiveLink href={"/blog"}>Blog</ActiveLink>
 
             <Button variant={"secondary"} asChild>
-              <Link href={"/comecar"}>
-               Começar
-              </Link>
+              <Link href={"/comecar"}>Começar</Link>
             </Button>
           </nav>
         </div>
